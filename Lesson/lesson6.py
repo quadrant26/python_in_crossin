12be3d10e5ea6bd3077e444c16005aaf5d2172a5
quadrant26 <quadrant26@163.com>
2018-01-03 15:18:23 +0800

    python 判断

---
+++ Lesson/lesson6.py
@@ -0,0 +1,24 @@
+a = 1 < 3
+print(a)
+
+b = 1
+c = 3
+print(b > c)
+
+print("--------------------------")
+
+num = 10
+print('Guess what I think?')
+answer = int(input())
+
+result = answer < num
+print('too small?')
+print(result)
+
+result = answer > num
+print('to big?')
+print(result)
+
+result = answer == num
+print('equal?')
+print(result)
