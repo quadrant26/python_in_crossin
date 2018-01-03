50a71458312fa453ca39a4d1fe8ab906bbd9f57b
quadrant26 <quadrant26@163.com>
2018-01-03 15:18:53 +0800

    python if task

---
+++ Lesson/lesson7_task.py
@@ -0,0 +1,10 @@
+print("Do you think am I?")
+result = input()
+if result == "cool":
+    print('Oh yes!')
+
+if result == 'stupid':
+    print('I\'m fine, Fuck you!')
+
+if result != 'stupid' or result != 'cool':
+    print('...')
