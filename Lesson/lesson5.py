2906ab4a8f1cc8671b68db8652f32f0b950f327d
quadrant26 <quadrant26@163.com>
2018-01-03 15:17:41 +0800

    python variable

---
+++ Lesson/lesson5.py
@@ -0,0 +1,14 @@
+name = 'Crossin'
+myVar = 123
+price = 5.99
+visible = True
+
+print(name)
+print(myVar)
+print(price)
+print(visible)
+
+print('Who do you think I am?')
+you = input()
+print('Oh, yes! I am a')
+print(you)
