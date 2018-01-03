215fbf8dc1cade7300bb56c38302c87ae3d26e49
quadrant26 <quadrant26@163.com>
2018-01-03 15:57:03 +0800

    python 类型转化

---
+++ Lesson/lesson17.py
@@ -0,0 +1,8 @@
+print('hello' + str(1))
+print("hello, %d" % int('123'))
+
+print(bool(-123))     # True
+print(bool(0))           # False
+print(bool('abc'))      # True
+print(bool('False'))    # True
+print(bool(''))            # False
