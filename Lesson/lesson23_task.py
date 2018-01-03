da87052cae9038e591ec54524aec1f98625d85b6
quadrant26 <quadrant26@163.com>
2018-01-03 16:15:10 +0800

    python def and if elif else

---
+++ Lesson/lesson23_task.py
@@ -0,0 +1,20 @@
+from random import randint
+
+print('Guess what I think?')
+num = randint(1, 100)
+bingo = False
+
+def isEqual(num1, num2):
+    if num1 < num2:
+        print("too small")
+        return False
+    elif num1 > num2:
+        print('too big')
+        return False
+    else:
+        print('bingo')
+        return True
+
+while bingo == False:
+    answer = int(input())
+    bingo = isEqual(answer, num)
