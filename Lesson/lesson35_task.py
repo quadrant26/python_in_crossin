1165e220f9e6290512fe532e350762e41f0cd709
quadrant26 <quadrant26@163.com>
2018-01-05 16:59:02 +0800

    break Test

---
+++ Lesson/lesson35_task.py
@@ -0,0 +1,18 @@
+i = 0
+while i < 5:
+    i += 1
+    
+    for j in range(3):
+        print(j)
+        if j == 2:
+            break
+
+    for k in range(3):
+        if k == 2:
+            break
+        print(k)
+
+    if i > 3:
+        break
+    print(i)
+    print('---------------')
