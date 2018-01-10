23b754cb27701918a7a91311b53578512ed11a1f
quadrant26 <quadrant26@163.com>
2018-01-10 15:12:20 +0800

    import 模块

--- Lesson/lesson38.py
+++ Lesson/lesson38.py
@@ -1,34 +1,13 @@
-from random import randint
+import random
 
-f = open('game.txt')
-score = f.read().split()
-print(score)
-f.close();
+# 查看模块详细信息
+dir(random)
 
+random.randint(1, 10)
+random.choice([1, 3, 5])
 
-game_times = int(score[0])      # 总游戏轮数
-min_times = int(score[1])       # 最快猜出的轮数
-total_times = int(score[2])     # 猜过的总轮数
+from math import pi
+# from math import pi as math_pi # 导入模块的别名
 
-# 计算平均轮数
-if game_times >0:
-    avg_times = float(total_times)/game_times
-else:
-    avg_times = 0
-
-print('你已经玩了 %d 次， 最少 %d 轮猜出答案， 平均 %.2f 轮猜出答案' % (game_times, min_times, avg_times))
-
-num = randint(1, 100)
-print('Guess what i think?')
-bingo = False
-
-while bingo == False:
-    answer = int(input("请输入您的数字："))
-    if answer < num:
-        print("Too small!")
-
-    if answer > num:
-        print('Too big')
-    if answer == num:
-        print('Bingo')
-        bingo = True
+# print math_pi
+print pi
